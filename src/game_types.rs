724e8141b67910a99f9c4083d13eab17f8e0348f
Mitchell Fream <m1fream@gmail.com>
2021-06-12 22:00:40 -0400

    third commit

---
+++ src/game_types.rs
@@ -0,0 +1,135 @@
+use std::ops;
+
+pub struct Game {
+	paused: bool,
+	tiles: [[Tile; Game::MAP_SIZE]; Game::MAP_SIZE],
+	resources: Resources,
+	production: Resources,
+	techs: [Tech; Tech::NUM_TECHS],
+//	upgrades: [Upgrade: Upgrade::NUM_UPGRADES],
+}
+
+impl Game {
+	pub const MAP_SIZE : usize = 255;
+}
+
+#[derive(Copy, Clone)]
+pub struct Resources {
+	_res: [f64; Resources::NUM_RESOURCES],
+}
+
+impl ops::Index<ResourceType> for Resources {
+	type Output = f64;
+	
+	fn index(self: &Resources, idx: ResourceType) -> &f64 {
+		return &self._res[idx as usize];
+	}
+}
+
+pub struct Tech {
+	name: String,
+	num: usize,
+	cost: Resources,
+	tier: usize,
+	desc: String,
+	unlocked: bool,
+	unlock: Box<Fn(Game) -> bool>,
+}
+
+impl Tech {
+	const NUM_TECHS : usize = 2;
+}
+
+pub struct Tile {
+	my_type: TileType,
+	unlocked: bool,
+}
+
+//#[derive(Eq, Hash)]
+pub enum ResourceType {
+	Wood = 0,
+	Food = 1,
+}
+
+pub enum TileType {
+	Empty,
+	Terrain(TerrainType),
+	Building(Building),
+}
+
+pub enum TerrainType {
+	Forest,
+	Lake,
+	Rock,
+}
+
+pub struct Building {
+	my_type: BuildingType,
+	level: u8,
+}
+
+enum BuildingType {
+	Farm
+}
+/*
+fn get_hover_str(x: TileType) -> String {
+	match x {
+		TileType::Empty => String::from("An empty space"),
+		TileType::Farm => String::from("A basic farm"),
+		TileType::Tree => String::from("Literally a tree"),
+	}
+}
+*/
+impl ops::Add<Resources> for Resources {
+	type Output = Resources;
+	
+	fn add(self, _rhs: Resources) -> Resources {
+		let mut new_res: [f64; Resources::NUM_RESOURCES] = [0.0; Resources::NUM_RESOURCES];
+		let mut i: usize = 0;
+		while(i<Resources::NUM_RESOURCES) {
+			new_res[i] = self._res[i] + _rhs._res[i];
+			i += 1;
+		}
+		Resources {
+			_res: new_res
+		}
+	}
+}
+
+impl ops::Sub<Resources> for Resources {
+	type Output = Resources;
+	
+	fn sub(self, _rhs : Resources) -> Resources {
+		self + (_rhs * -1.0)
+	}
+}
+
+impl ops::Mul<f64> for Resources {
+	type Output = Resources;
+	
+	fn mul(self: Resources, _rhs: f64) -> Resources {
+		let mut new_res: [f64; Resources::NUM_RESOURCES] = [0.0; Resources::NUM_RESOURCES];
+		let mut i: usize = 0;
+		while(i<Resources::NUM_RESOURCES) {
+			new_res[i] = self._res[i] * _rhs;
+			i += 1;
+		}
+		Resources {
+			_res: new_res
+		}
+	}
+}
+
+impl Resources {
+	pub const NUM_RESOURCES : usize = 2;
+	pub fn can_buy(self, cost: Resources) -> bool {
+		let mut i: usize = 0;
+		while(i<Resources::NUM_RESOURCES) {
+			if(cost._res[i] > self._res[i]) {
+				return false;
+			}
+			i += 1;
+		}
+		return true;
+	}
+}
--- src/production.rs
+++ src/production.rs
@@ -1,3 +1,22 @@
+use crate::game_types;
+
 pub fn test() -> usize {
-  return 2;
+	return 2;
 }
+
+fn prod_nothing(g: game_types::Game, i: usize, j: usize) {
+	return game_types::Resources {};
+}
+
+//let prod_funcs : [fn(game_types::Game, usize, usize); game_types::Resources::NUM_RESOURCES] = [prod_nothing; game_types::Resources::NUM_RESOURCES];
+
+let prod_funcs = [prod_nothing; game_types::Resources::NUM_RESOURCES];
+
+/*
+pub fn calc_production(g: game_types::Game, step: f64) -> game_types::Resources {
+	let mut i: usize = 0;
+	let mut j: usize = 0;
+	while(i<game_types::Game::MAP_SIZE) {
+		while(j<game_types::Game::MAP_SIZE) {
+			
+}*/
