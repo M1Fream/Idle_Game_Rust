fa591b3ff8537fe4410cebd116b7796a9b8d1813
Mitchell Fream <m1fream@gmail.com>
2021-06-12 22:58:59 -0400

    fourth commit

--- src/production.rs
+++ src/production.rs
@@ -4,13 +4,17 @@ pub fn test() -> usize {
 	return 2;
 }
 
-fn prod_nothing(g: game_types::Game, i: usize, j: usize) {
-	return game_types::Resources {};
+fn prod_nothing(g: game_types::Game, i: usize, j: usize) -> game_types::Resources {
+	return game_types::Resources::new(Vec::new());
+}
+/*
+fn prod_basic_factory(res: &'static game_types::Resources) -> (Box<Fn(game_types::Game, usize, usize) -> game_types::Resources>) {
+	return Box::new(|g: game_types::Game, i: usize, j: usize| -> game_types::Resources {return *res});
 }
 
-//let prod_funcs : [fn(game_types::Game, usize, usize); game_types::Resources::NUM_RESOURCES] = [prod_nothing; game_types::Resources::NUM_RESOURCES];
-
-let prod_funcs = [prod_nothing; game_types::Resources::NUM_RESOURCES];
+static prod_funcs : [Box<Fn(game_types::Game, usize, usize) -> game_types::Resources>; game_types::Resources::NUM_RESOURCES] = [Box::new(prod_nothing); game_types::Resources::NUM_RESOURCES];
+*/
+//let prod_funcs = [prod_nothing; game_types::Resources::NUM_RESOURCES];
 
 /*
 pub fn calc_production(g: game_types::Game, step: f64) -> game_types::Resources {
