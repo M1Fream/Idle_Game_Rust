332d40167346db966ad3eaccc4e0a7d5011cfbeb
Mitchell Fream <m1fream@gmail.com>
2021-06-22 22:44:36 -0400

    production refactor, it's better this way

--- src/production.rs
+++ src/production.rs
@@ -12,9 +12,9 @@ trait production_obj {
 }
 
 trait Bonus {
-	fn calc_ratio(&self, g: &game_types::Game, i: usize, j: usize) -> f64;
+	fn calc_ratio(&self, g: &game_types::Game, tile : world::Tile) -> f64;
 }
-
+/*
 trait StandardBonuses<'a> {
 	fn get_edge_bonuses(&self) -> &Vec<&'a Bonus>;
 	fn get_corner_bonuses(&self) -> &Vec<&'a Bonus>;
@@ -39,26 +39,40 @@ trait StandardBonuses<'a> {
 		return mult;
 	}
 }
-
+*/
+trait HasBonus<'a> {
+	fn get_bonuses(&self) -> &Vec<&'a Bonus>;
+	fn calc_bonuses(&self, g: &game_types::Game, tile: world::Tile) -> f64 {
+		let mut ret = 1.0;
+		for bonus in self.get_bonuses() {
+			ret *= bonus.calc_ratio(g, tile);
+		}
+		return ret;
+	}
+}
+/*
 struct SimpleTileBonus {
 	pub ratio: f64,
 	pub tile: world::TileType,
-}
+}*/
 
 struct TileBonus<'a> {
 	pub ratio: f64,
 	pub tile: world::TileType,
-	edge_bonuses: Vec<&'a Bonus>,
-	corner_bonuses: Vec<&'a Bonus>,
-	upgrade_bonuses: Vec<&'a Bonus>,
+//	edge_bonuses: Vec<&'a Bonus>,
+//	corner_bonuses: Vec<&'a Bonus>,
+//	upgrade_bonuses: Vec<&'a Bonus>,
+	bonuses: Vec<&'a Bonus>,
+	get_tiles: Fn(world::WorldMap, world::Tile) -> Vec<world::Tile>,
 }
-
+/*
 impl<'a> StandardBonuses<'a> for TileBonus<'a> {
 	fn get_edge_bonuses(&self) -> &Vec<&'a Bonus> {return &self.edge_bonuses}
 	fn get_corner_bonuses(&self) -> &Vec<&'a Bonus> {return &self.corner_bonuses}
 	fn get_upgrade_bonuses(&self) -> &Vec<&'a Bonus> {return &self.upgrade_bonuses}
 }
-
+*/
+/*
 impl Bonus for SimpleTileBonus {
 	fn calc_ratio(&self, g: &game_types::Game, i: usize, j: usize) -> f64 {
 		let t = self.tile;
@@ -68,38 +82,52 @@ impl Bonus for SimpleTileBonus {
 			return 1.0;
 		}
 	}
-}
+}*/
 
 impl<'a> Bonus for TileBonus<'a> {
-	fn calc_ratio(&self, g: &game_types::Game, i: usize, j: usize) -> f64 {
+	fn calc_ratio(&self, g: &game_types::Game, tile: world::Tile) -> f64 {
 		let t = self.tile;
-		if matches!(g.world_map.tiles[i][j].my_type, t) {
-			return self.ratio * self.calc_bonuses(g, i, j);
+		if matches!(tile.my_type, t) {
+			return self.ratio * self.calc_bonuses(g, tile);
 		} else {
 			return 1.0;
 		}
 	}
 }
 
-struct basic_production<'a> {
-	pub base: game_types::Resources,
-	pub edge_bonuses: Vec<&'a Bonus>,
-	pub corner_bonuses: Vec<&'a Bonus>,
-	pub upgrade_bonuses: Vec<&'a Bonus>,
-	pub tier: usize,
-}
-
-impl<'a> basic_production<'a> {
-	pub fn new(res: game_types::Resources, edge: Vec<&'a Bonus>, corner: Vec<&'a Bonus>, upgrade: Vec<&'a Bonus>, tier : usize) -> basic_production<'a> {
-		return basic_production {base: res, edge_bonuses: edge, corner_bonuses: corner, upgrade_bonuses: upgrade, tier: tier};
+impl<'a> HasBonus<'a> for TileBonus<'a> {
+	fn get_bonuses(&self) -> &Vec<&'a Bonus> {
+		return &self.bonuses;
 	}
 }
 
+struct basic_production<'a> {
+	pub base: game_types::Resources,
+//	pub edge_bonuses: Vec<&'a Bonus>,
+//	pub corner_bonuses: Vec<&'a Bonus>,
+//	pub upgrade_bonuses: Vec<&'a Bonus>,
+	pub bonuses: Vec<&'a Bonus>,
+	pub tier: usize,
+}
+
+impl<'a> HasBonus<'a> for basic_production<'a> {
+	fn get_bonuses(&self) -> &Vec<&'a Bonus> {
+		return &self.bonuses;
+	}
+}
+
+impl<'a> basic_production<'a> {
+	pub fn new(res: game_types::Resources, bonuses: Vec<&'a Bonus>, tier : usize) -> basic_production<'a> {
+		//return basic_production {base: res, edge_bonuses: edge, corner_bonuses: corner, upgrade_bonuses: upgrade, tier: tier};
+		return basic_production {base: res, bonuses: bonuses, tier: tier};
+	}
+}
+/*
 impl<'a> StandardBonuses<'a> for basic_production<'a> {
 	fn get_edge_bonuses(&self) -> &Vec<&'a Bonus> {return &self.edge_bonuses}
 	fn get_corner_bonuses(&self) -> &Vec<&'a Bonus> {return &self.corner_bonuses}
 	fn get_upgrade_bonuses(&self) -> &Vec<&'a Bonus> {return &self.upgrade_bonuses}
-}
+}*/
 
 impl<'a> production_obj for basic_production<'a> {
 	fn calc_prod(&self, g: &game_types::Game, i: usize, j:usize) -> game_types::Resources {
@@ -128,9 +156,9 @@ fn init_prod_objs() ->  ArrayVec<Box<dyn production_obj>, { game_types::Building
 //	let mut ret_vec : ArrayVec<Box<dyn production_obj>> = ArrayVec::with_capacity(game_types::Resources::NUM_RESOURCES);
 	let mut ret_vec : ArrayVec<Box<dyn production_obj>, { game_types::Building::NUM_BUILDINGS }> = ArrayVec::<_, {game_types::Building::NUM_BUILDINGS}>::new();
 	for _ in 0..game_types::Building::NUM_BUILDINGS {
-		ret_vec.push(Box::new(basic_production::new(game_types::Resources::new(Vec::new()), Vec::new(), Vec::new(), Vec::new(), 1)));
+		ret_vec.push(Box::new(basic_production::new(game_types::Resources::new(Vec::new()), Vec::new(), 1)));
 	} //we initialize everything
-	ret_vec[game_types::BuildingType::Farm] = Box::new(basic_production::new(game_types::Resources::new(vec![(game_types::ResourceType::Food, 5.0)]), Vec::new(), Vec::new(), Vec::new(), 1));
+	ret_vec[game_types::BuildingType::Farm] = Box::new(basic_production::new(game_types::Resources::new(vec![(game_types::ResourceType::Food, 5.0)]), Vec::new(), 1));
 	return ret_vec;
 }
 
--- src/world.rs
+++ src/world.rs
@@ -5,10 +5,17 @@ use crate::game_types;
 pub struct Tile {
 	pub my_type: TileType,
 	unlocked: bool,
+	x: usize,
+	y: usize,
+}
+
+pub fn get_edge_neighbors(map: WorldMap, tile: Tile) -> Vec<Tile> {
+	return Vec::new();
 }
 
 pub struct WorldMap {
-	pub tiles : [[Tile; WorldMap::MAP_SIZE]; WorldMap::MAP_SIZE]
+//	pub tiles : [[Tile; WorldMap::MAP_SIZE]; WorldMap::MAP_SIZE]
+	pub tiles : Vec<Vec<Tile>>,
 }
 
 #[derive(Copy, Clone)]
@@ -36,12 +43,33 @@ impl TerrainType{
 }
 
 impl WorldMap {
-	pub const MAP_SIZE : usize = 255;
+	pub const MAP_SIZE : usize = 63;
 
 	/*pub fn new() -> WorldMap{
 		//Add worldgen here
 		return 
 	}*/
+	pub fn get_edge_neighbors(self, tile: Tile) -> Vec<Tile> {
+		let mut ret : Vec<Tile> = Vec::new();
+		let x = tile.x;
+		let y = tile.y;
+		if x!=0 {ret.push(self.tiles[x-1][y])}
+		if y!=0 {ret.push(self.tiles[x][y-1])}
+		if x!=WorldMap::MAP_SIZE {ret.push(self.tiles[x+1][y])}
+		if y!=WorldMap::MAP_SIZE {ret.push(self.tiles[x][y+1])}
+		return ret;
+	}
+	
+	pub fn get_corner_neighbors(self, tile: Tile) -> Vec<Tile> {
+		let mut ret : Vec<Tile> = Vec::new();
+		let x = tile.x;
+		let y = tile.y;
+		if x!=0 && y!= 0 {ret.push(self.tiles[x-1][y-1])}
+		if x!=0 && y!= WorldMap::MAP_SIZE {ret.push(self.tiles[x-1][y+1])}
+		if x!=WorldMap::MAP_SIZE && y!= 0 {ret.push(self.tiles[x+1][y-1])}
+		if x!=WorldMap::MAP_SIZE && y!=WorldMap::MAP_SIZE {ret.push(self.tiles[x+1][y+1])}
+		return ret;
+	}
 }
 
 impl Default for TileType {
@@ -55,12 +83,21 @@ impl Default for Tile {
 		Self {
 			my_type: TileType::default(),
 			unlocked: false,
+			x: 0,
+			y: 0,
 		}
 	}
 }
 
 impl Default for WorldMap {
 	fn default() -> Self {
-		Self {tiles: [[Tile::default(); WorldMap::MAP_SIZE]; WorldMap::MAP_SIZE]}
+		let mut tiles : Vec<Vec<Tile>> = Vec::new();
+		for i in 0..WorldMap::MAP_SIZE {
+			tiles.push(Vec::new());
+			for j in 0..WorldMap::MAP_SIZE {
+				tiles[i].push(Tile {my_type: TileType::default(), unlocked: false, x: i, y:j});
+			}
+		}
+		Self {tiles: tiles}
 	}
 }
